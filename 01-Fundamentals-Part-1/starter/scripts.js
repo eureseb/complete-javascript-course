9fbed79a130e51c27005925a06a9ab3f9a9d817d
eureseb <euresebb@gmail.com>
2022-02-15 16:07:38 +0800

    intro to data types

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -4,3 +4,15 @@ if(js === 'amazing')
     alert('Hello world!');
 
 console.log((10.101001).toFixed(2));
+
+let firstName = "Eurese"
+
+console.log(firstName[0].toUpperCase());
+
+let variable = true;
+
+console.log(typeof variable)
+
+variable = [1, 2, 3, 4];
+
+console.log(variable);
